516866a3673e3423c676caf973006856aa42bcf9
sedrawael <gyrfhi123@gmail.com>
2025-04-16 09:29:41 +0300

    new

---
+++ first_project/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/birthdayCard.png'),  
+              fit: BoxFit.cover, 
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
